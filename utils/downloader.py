6e6594f836671fb1254489c7f996e5aeba963a27
pastelite <kana.bhong@gmail.com>
2024-03-29 23:53:46 +0700

    adding more videos and adding progress bar to extractor

--- utils/downloader.py
+++ utils/downloader.py
@@ -1,98 +1,98 @@
 from pytube import YouTube
+from pytube.cli import on_progress
 import os.path as path
 import os
 import warnings
 import pathlib
 import unittest
 
-# class VideoDownloaderBase:
-#     output_dir:str
-    
-#     def __init__(self, output_dir = "./download") -> None:
-#         self.output_dir = output_dir
-        
-#         # check if output_dir exists
-#         if path.exists(output_dir) == False:
-#             warnings.warn(f"Output directory {output_dir} does not exist. creating...")
-#             os.makedirs(output_dir)
-        
-#     def download(self, link:str, output_name:str|None = None):
-#         pass
-        
-# class YouTubeDownloader(VideoDownloaderBase):
-#     def download(self, link:str, output_name:str|None = None):
-#         yt = YouTube(link);
-#         if output_name:
-#             filename = path.join(self.output_dir, output_name+".mp4")
-#         else:
-#             filename = path.join(self.output_dir, yt.video_id+".mp4")
-        
-#         stream = yt.streams.filter(progressive=True, file_extension='mp4').get_lowest_resolution()
-#         if stream:
-#             stream.download(filename=filename)
-#         else:
-#             warnings.warn(f"The video {yt.title} has no stream found")
 
-def download_youtube_video(link:str, output_path = "./download", no_create_dir = False):
-    yt = YouTube(link);
+def download_youtube_video(
+    link: str, output_path="./download", no_create_dir=False, show_progress=False
+):
+    if show_progress:
+        print(f"Downloading {link} to {output_path}")
+    yt = YouTube(link, on_progress_callback=on_progress if show_progress else None)
     path = pathlib.Path(output_path)
-    
+
+    if path.exists() and path.is_file():
+        print(f"Output file {output_path} is already exists, skipping...")
+        return
+
     if path.is_dir():
         if not path.exists():
             if no_create_dir:
-                raise FileNotFoundError(f"Output directory {output_path} does not exist.")
+                raise FileNotFoundError(
+                    f"Output directory {output_path} does not exist."
+                )
             warnings.warn(f"Output directory {output_path} does not exist. creating...")
             path.mkdir(parents=True)
-        filename = path / (yt.video_id+".mp4")
+        filename = path / (yt.video_id + ".mp4")
     else:
         if not path.parent.exists():
             if no_create_dir:
-                raise FileNotFoundError(f"Output directory {output_path} does not exist.")
+                raise FileNotFoundError(
+                    f"Output directory {output_path} does not exist."
+                )
             warnings.warn(f"Output directory {path.parent} does not exist. creating...")
             path.parent.mkdir(parents=True)
         filename = path
         if path.suffix == "":
             filename = path.with_suffix(".mp4")
-        
-    
-    stream = yt.streams.filter(progressive=True, file_extension="mp4").get_lowest_resolution()
+
+    stream = yt.streams.filter(
+        progressive=True, file_extension="mp4"
+    ).get_lowest_resolution()
     if stream:
         stream.download(filename=str(filename))
     else:
         warnings.warn(f"The video {yt.title} has no stream found")
-        
+
+
 class TestDownloadYoutubeVideo(unittest.TestCase):
     def test_download_no_ext(self):
         os.makedirs("./download_test", exist_ok=True)
-        download_youtube_video("https://www.youtube.com/watch?v=fxqE27gIZcc", "./download_test/test")
+        download_youtube_video(
+            "https://www.youtube.com/watch?v=fxqE27gIZcc", "./download_test/test"
+        )
         self.assertTrue(path.exists("./download_test/test.mp4"))
         os.remove("./download_test/test.mp4")
         os.rmdir("./download_test")
-        
+
     def test_download_ext(self):
         os.makedirs("./download_test", exist_ok=True)
-        download_youtube_video("https://www.youtube.com/watch?v=fxqE27gIZcc", "./download_test/test.mp4")
+        download_youtube_video(
+            "https://www.youtube.com/watch?v=fxqE27gIZcc", "./download_test/test.mp4"
+        )
         self.assertTrue(path.exists("./download_test/test.mp4"))
         os.remove("./download_test/test.mp4")
         os.rmdir("./download_test")
-        
+
     def test_download_auto_path(self):
         with self.assertWarns(UserWarning):
-            download_youtube_video("https://www.youtube.com/watch?v=fxqE27gIZcc", "./download_test/test.mp4")
+            download_youtube_video(
+                "https://www.youtube.com/watch?v=fxqE27gIZcc",
+                "./download_test/test.mp4",
+            )
         self.assertTrue(path.exists("./download_test/test.mp4"))
         os.remove("./download_test/test.mp4")
         os.rmdir("./download_test")
-        
+
     def test_download_no_create_dir(self):
         with self.assertRaises(FileNotFoundError):
-            download_youtube_video("https://www.youtube.com/watch?v=fxqE27gIZcc", "./download_test_not_exists/test", no_create_dir=True)
-    
+            download_youtube_video(
+                "https://www.youtube.com/watch?v=fxqE27gIZcc",
+                "./download_test_not_exists/test",
+                no_create_dir=True,
+            )
+
     def test_download_youtudotbe_only(self):
         os.makedirs("./download_test", exist_ok=True)
         download_youtube_video("youtu.be/fxqE27gIZcc", "./download_test/test")
         self.assertTrue(path.exists("./download_test/test.mp4"))
         os.remove("./download_test/test.mp4")
         os.rmdir("./download_test")
-            
+
+
 if __name__ == "__main__":
     unittest.main()
--- utils/extractor.py
+++ utils/extractor.py
@@ -1,42 +1,157 @@
 import cv2
 import os
 from pathlib import Path
+from tqdm import tqdm
 
-def extract_frames(video_path: str, output_dir: str, video_name: str|None=None, interval_ms: int = 1000):
-    """Extracts frames from a video at a specified interval."""
+# def extract_frames(video_path: str, output_dir: str, video_name: str|None=None, interval_ms: int = 1000):
+#     """Extracts frames from a video at a specified interval."""
+
+#     # Make sure output directory exists
+#     if not os.path.exists(output_dir):
+#         os.makedirs(output_dir, exist_ok=True)
+#         print(f"Output directory {output_dir} does not exist. creating...")
+
+#     # Check if video exists
+#     if not os.path.exists(video_path):
+#         raise FileNotFoundError(f"Video file not found: {video_path}")
+
+#     video = cv2.VideoCapture(video_path)
+
+#     try:
+#         # Calculate fps and frame skip
+#         fps = video.get(cv2.CAP_PROP_FPS)
+#         frame_skip = int(fps * interval_ms / 1000)
+
+#         # Extracting
+#         frame_i = 0
+#         while True:
+#             ret, frame = video.read()
+#             if not ret:
+#                 break
+
+#             if frame_i % frame_skip == 0:
+#                 # vid_name = video_name if video_name else os.path.basename(video_path).split(".")[0]
+#                 vid_name = video_name if video_name else Path(video_path).stem
+#                 filename = f"{vid_name}_{frame_i:04d}.jpg"
+#                 cv2.imwrite(os.path.join(output_dir, filename), frame)
+
+#             frame_i += 1
+
+#         print(f"Extracted {int(frame_i/frame_skip)} frames at {interval_ms}ms interval (originally {frame_i} frames)")
+
+#     finally:
+#         video.release()
+
+
+def extract_frames_interval(
+    video_path: str, output_dir: str, name: str | None = None, interval=30
+) -> tuple[int, int, int]:
+    """
+    Extract frames from a video at a specified frame interval
+
+    Returns
+    -------
+    tuple[int,int,int]
+        (number of frames extracted, total number of frames, interval)
+    """
 
     # Make sure output directory exists
     if not os.path.exists(output_dir):
         os.makedirs(output_dir, exist_ok=True)
         print(f"Output directory {output_dir} does not exist. creating...")
-        
+
     # Check if video exists
     if not os.path.exists(video_path):
         raise FileNotFoundError(f"Video file not found: {video_path}")
 
     video = cv2.VideoCapture(video_path)
+    vid_name = name if name else Path(video_path).stem
 
     try:
-        # Calculate fps and frame skip
-        fps = video.get(cv2.CAP_PROP_FPS)
-        frame_skip = int(fps * interval_ms / 1000)
-
         # Extracting
-        frame_i = 0
-        while True:
+        # frame_i = 0
+        # while True:
+        num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        # num_frames_extract = num_frames // interval
+        for frame_i in (progress_bar := tqdm(range(0, num_frames, interval))):
+            # skipping frames
+            # frame_i = set_i * interval
+            progress_bar.set_description(f"{vid_name} [{frame_i}/{num_frames}]")
+            video.set(cv2.CAP_PROP_POS_FRAMES, frame_i)
+
             ret, frame = video.read()
             if not ret:
                 break
 
-            if frame_i % frame_skip == 0:
+            if frame_i % interval == 0:
                 # vid_name = video_name if video_name else os.path.basename(video_path).split(".")[0]
-                vid_name = video_name if video_name else Path(video_path).stem
                 filename = f"{vid_name}_{frame_i:04d}.jpg"
                 cv2.imwrite(os.path.join(output_dir, filename), frame)
+                # progress_bar.set_postfix_str(f"n={int(frame_i/interval)+1}")
 
-            frame_i += 1
-
-        print(f"Extracted {int(frame_i/frame_skip)} frames at {interval_ms}ms interval (originally {frame_i} frames)")
+        # print(f"Extracted {int(frame_i/interval)} frames at {interval} frames interval (originally {frame_i} frames)")
 
     finally:
         video.release()
+        return int(frame_i / interval) + 1, num_frames, interval
+
+
+def extract_n_frames(
+    video_path: str, output_dir: str, video_name: str | None = None, n: int = 0
+):
+    """
+    Extract frame_n frames from a video
+
+    Returns
+    -------
+    tuple[int,int,int]
+        (number of frames extracted, total number of frames, interval)
+    """
+    if n <= 0:
+        return 0, 0, 0
+
+    if not os.path.exists(video_path):
+        raise FileNotFoundError(f"Video file not found: {video_path}")
+
+    video = cv2.VideoCapture(video_path)
+    interval_frame = int(video.get(cv2.CAP_PROP_FRAME_COUNT)) // (
+        n - 1
+    )  # n-1 because we start from 0
+
+    return extract_frames_interval(video_path, output_dir, video_name, interval_frame)
+
+
+def extract_frames_time_interval(
+    video_path: str,
+    output_dir: str,
+    video_name: str | None = None,
+    interval: int = 1000,
+):
+    """
+    Extract frames from a video at a specified time interval
+
+    Parameters
+    ----------
+    video_path : str
+        Path to the video file
+    output_dir : str
+        Path to the output directory
+    video_name : str, optional
+        Name of the video that will appended to filename, defaulting to the video file name
+    interval : int, optional
+        Time interval in milliseconds, defaulting to 1000 (1 second)
+
+    Returns
+    -------
+    tuple[int,int,int]
+        (number of frames extracted, total number of frames, interval)
+    """
+
+    if not os.path.exists(video_path):
+        raise FileNotFoundError(f"Video file not found: {video_path}")
+
+    video = cv2.VideoCapture(video_path)
+    fps = video.get(cv2.CAP_PROP_FPS)
+    interval_frame = int(fps * interval / 1000)
+
+    return extract_frames_interval(video_path, output_dir, video_name, interval_frame)
